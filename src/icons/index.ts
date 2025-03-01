3e1cc644f8a4ee01acf4dffebdaf5fff78cfea0d
Manuel Gonzalez Garcia <mgonzalez4648@gmail.com>
2025-03-01 18:49:41 +0100

    Create files for SVG skill images

---
+++ src/icons/index.ts
@@ -0,0 +1,11 @@
+export { default as ReactIcon } from './ReactIcon.astro';
+export { default as TypeScriptIcon } from './TypeScriptIcon.astro';
+export { default as CSSIcon } from './CSSIcon.astro';
+export { default as NodeIcon } from './NodeIcon.astro';
+export { default as PythonIcon } from './PythonIcon.astro';
+export { default as SQLIcon } from './SQLIcon.astro';
+export { default as DockerIcon } from './DockerIcon.astro';
+export { default as AWSIcon } from './AWSIcon.astro';
+export { default as GitIcon } from './GitIcon.astro';
+export { default as VueIcon } from './VueIcon.astro';
+export { default as NextIcon } from './NextIcon.astro';
