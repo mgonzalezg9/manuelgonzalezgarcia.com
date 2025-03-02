575be6ad948de7d27298ad9e032c1a1b534f6935
Manuel Gonzalez Garcia <mgonzalez4648@gmail.com>
2025-03-02 18:35:10 +0100

    Improve Technologies section

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,10 +1,7 @@
 export { default as ReactIcon } from './ReactIcon.astro';
 export { default as TypeScriptIcon } from './TypeScriptIcon.astro';
-export { default as CSSIcon } from './CSSIcon.astro';
 export { default as NodeIcon } from './NodeIcon.astro';
 export { default as PythonIcon } from './PythonIcon.astro';
-export { default as SQLIcon } from './SQLIcon.astro';
-export { default as DockerIcon } from './DockerIcon.astro';
 export { default as AWSIcon } from './AWSIcon.astro';
 export { default as GitIcon } from './GitIcon.astro';
 export { default as VueIcon } from './VueIcon.astro';
